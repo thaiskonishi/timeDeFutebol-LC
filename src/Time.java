44322911c2979f2734615c7309908cc533a87ef0
ThaisMK <thais.konishi@gmail.com>
2022-07-20 15:08:47 -0300

    criacao das classes

---
+++ src/Time.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class Time {
+
+    private String nomeTime;
+    private List<Jogador> time;
+
+    public String getNomeTime() {
+        return nomeTime;
+    }
+
+    public List<Jogador> getTime() {
+        return time;
+    }
+
+    public void setTime(List<Jogador> time) {
+        this.time = time;
+    }
+
+}
